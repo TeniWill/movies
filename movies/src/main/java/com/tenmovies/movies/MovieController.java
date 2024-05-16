e2d820496d521ccdfab1a0aa83bdef88a0b37070
TeniWill <teniwilliams7@gmail.com>
2024-05-16 23:10:34 +0100

    second commit

--- movies/src/main/java/com/tenmovies/movies/MovieController.java
+++ movies/src/main/java/com/tenmovies/movies/MovieController.java
@@ -1,9 +1,10 @@
 package com.tenmovies.movies;
 
-import org.bson.types.ObjectId;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/v1/movies")
 public class MovieController {
     @Autowired
